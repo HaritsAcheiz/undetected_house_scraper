91c4e8fb5e208a763deb1159edd4566020652654
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-10-25 06:15:01 +0700

    add profile and proxy. not stable

--- scraper.py
+++ scraper.py
@@ -1,21 +1,25 @@
 import undetected_chromedriver as uc
 from dataclasses import dataclass
-
+from selectolax.parser import HTMLParser
 from undetected_chromedriver import ChromeOptions
+from urllib.parse import urljoin
 
 
 @dataclass
 class Scraper():
-
+    base_url: str = 'https://www.zillow.com'
 
 
     def webdriversetup(self):
+        PROXY = '154.12.112.208:8800'
         opt = ChromeOptions()
         opt.add_argument("--start-maximized")
         opt.add_argument("--headless=new")
         opt.add_argument("--no-sandbox")
-        opt.add_argument(r'--user-data-dir=D:\Naru\undetected_house_scraper\User Data')
-        opt.add_argument(r'--profile-directory=Profile 1')
+        opt.add_argument("--user-data-dir=/home/haritz/snap/chromium/common/chromium")
+        opt.add_argument(r'--profile-directory=Default')
+        opt.add_argument(f'--proxy-server={PROXY}')
+        opt.add_argument("--disable-blink-features=AutomationControlled")
 
         driver = uc.Chrome(options=opt)
 
@@ -25,13 +29,25 @@ class Scraper():
         driver.get(url)
         html = driver.page_source
         driver.close()
-
+        print(html)
         return html
+
+    def get_listing_link(self, html):
+        tree = HTMLParser(html)
+        listings = tree.css('li.StyledListCardWrapper-srp__sc-wtsrtn-0')
+        links=[]
+        for listing in listings:
+            print(listing.html)
+            endpoint = listing.css_first('a').attributes.get('href', '')
+            link = urljoin(self.base_url, endpoint)
+            links.append(link)
+        print(f'{len(links)} link(s) collected!')
+        print(links)
 
     def main(self):
         driver = self.webdriversetup()
-        html = self.fetch_html(driver=driver, url='https://www.zillow.com/tucson-az/rent-houses/')
-        print(html)
+        html = self.fetch_html(driver=driver, url='https://www.zillow.com/homes/Tucson,-AZ_rb/')
+        self.get_listing_link(html)
 
 if __name__ == '__main__':
     s = Scraper()
